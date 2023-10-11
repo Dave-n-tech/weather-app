e481a250427aad3d4d43d6a81d8fb420136c927e
David <davefast2003@gmail.com>
2023-10-10 19:20:34 -0700

    added  error handling and event-handler for enter key click

--- weather-app.js
+++ weather-app.js
@@ -29,61 +29,74 @@ const createWeatherCard = (weatherItem) => {
 
 //async function to get current weather data from the api 
 const getWeather = async (city) => {
-  const response = await fetch(apiUrl + `key=${apiKey}&q=${city}&aqi=yes`);
-  let data = await response.json();
-  //console.log(data)
-
-  cityName.textContent = data.location.name;
-  presentDate.textContent = data.location.localtime.slice(0, 11);
+  try{
+    const response = await fetch(apiUrl + `key=${apiKey}&q=${city}&aqi=yes`);
+    if (!response.ok) {
+      throw new Error(`HTTP error! Status: ${response.status}`);
+    }
+    const data = await response.json();
+    console.log(data)
   
-  // selecting only the needed properties from the current obects by destructuring and inserting into the html elements
-  const { temp_c, wind_mph, humidity } = data.current;
-  details[0].textContent = `Temperature: ${temp_c}C`;
-  details[1].textContent = `Wind: ${wind_mph}mph`;
-  details[2].textContent = `Humidity: ${humidity}%`;
-
-  icon.src = data.current.condition.icon;
-  description.textContent = data.current.condition.text;
+    cityName.textContent = data.location.name;
+    presentDate.textContent = data.location.localtime.slice(0, 11);
+    
+    // selecting only the needed properties from the current obects by destructuring and inserting into the html elements
+    const { temp_c, wind_mph, humidity } = data.current;
+    details[0].textContent = `Temperature: ${temp_c}C`;
+    details[1].textContent = `Wind: ${wind_mph}mph`;
+    details[2].textContent = `Humidity: ${humidity}%`;
+    
+    icon.src = data.current.condition.icon;
+    description.textContent = data.current.condition.text; 
+  }catch(error){
+    console.error("Error:", error)
+    alert("An error has occured, please try again")
+  }
 };
 
 
 //async function to get the forecast weather data from api
 const getForecast = async (city) => {
-  const response = await fetch(
-    forecastUrl + `key=${apiKey}&q=${city}&aqi=yes&days=5`
-  );
-  let data = await response.json();
-  // selecting only the forecast days
-  let threeDayForecast = data.forecast.forecastday;
-  console.log(threeDayForecast);
-  //clearing the forecast container
-  forecastBox.innerHTML = "";
-  // looping through the forecast days and creating a card display for each of them
-  threeDayForecast.forEach((weatherItem) => {
-    forecastBox.insertAdjacentHTML("beforeend", createWeatherCard(weatherItem));
-  });
+  try{
+    const response = await fetch(
+      forecastUrl + `key=${apiKey}&q=${city}&aqi=yes&days=5`
+    );
+    let data = await response.json();
+    // selecting only the forecast days
+    let threeDayForecast = data.forecast.forecastday;
+    console.log(threeDayForecast);
+    //clearing the forecast container
+    forecastBox.innerHTML = "";
+    // looping through the forecast days and creating a card display for each of them
+    threeDayForecast.forEach((weatherItem) => {
+      forecastBox.insertAdjacentHTML("beforeend", createWeatherCard(weatherItem));
+    });
+  }catch(error){
+    console.error('Error:', error)
+  }
 };
 
 
 // function to get the city input value and call the current and forecast weather functions
-const generateWeather = (error) => {
-  error.preventDefault;
+const generateWeather = (event) => {
+  event.preventDefault();
   if(input.value.length === 0){
     alert("please enter a city name")
+  }else{
+    const city = input.value;
+    getWeather(city);
+    input.value = "";
+    getForecast(city);
   }
-  const city = input.value;
-  getWeather(city);
-  input.value = "";
-  getForecast(city);
 };
 
 // event listener on button to call the generate weather function
 button.addEventListener("click", generateWeather);
-/*
+
 input.addEventListener("keypress", (e)=> {
 
   if(e.key === 'Enter'){
-    generateWeather()
+    generateWeather(e)
     input.value = ""
   }
-})*/
+})
